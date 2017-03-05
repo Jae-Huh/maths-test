aa033a67cc7eef4af631ae930c6161bb01017356
Jae Huh <jaeeunhuh@gmail.com>
2017-03-05 23:13:54 +1300

    Set up javascript and add function to submit button

---
+++ script.js
@@ -0,0 +1,9 @@
+var ans1 = document.querySelector('.a1');
+var button = document.querySelector('#btn')
+
+button.addEventListener('click', function() {
+  if (ans1.value === '15') {
+    console.log("correct")
+  }
+  console.log("hey")
+})
